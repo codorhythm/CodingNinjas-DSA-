5066fa3e9e37c9daaedde9825f330dd9cb06f632
Pratulya Pratap <pratulyapratap@gmail.com>
2023-01-21 12:12:58 +0530

    Added a node file

---
+++ linkedList1/LinkedListNode.java
@@ -0,0 +1,14 @@
+package linkedList1;
+
+public class LinkedListNode<T> {
+	T data;
+	LinkedListNode next;
+	
+	public LinkedListNode(T data) 
+	{
+		// TODO Auto-generated constructor stub
+		this.data = data;
+		next = null;
+	}
+
+}
